f7eee8c3c42ef04a93711039d37f434ebd02281a
Henry Recker <henryrecker@pingidentity.com>
2024-01-19 13:46:06 -0600

    Scaffolding

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"adventofcode2024/solutions"
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Hello, world.")
+
+	solutions.Day1Part1("input")
+}
---
+++ solutions/day1.go
@@ -0,0 +1,7 @@
+package solutions
+
+import "fmt"
+
+func Day1Part1(input string) {
+	fmt.Println("Yay part 1")
+}
